833a7f71405c339d65f879ef208cc134b7f17b96
Anantha Halmuttur <34939884+bigboy32@users.noreply.github.com>
2021-02-22 09:33:26 +0100

    Fix 'brand' error.

--- scripts/sysinfo.py
+++ scripts/sysinfo.py
@@ -1,7 +1,7 @@
 import cpuinfo
 
 def is_amd():
-    brand = cpuinfo.get_cpu_info()['brand']
+    brand = cpuinfo.get_cpu_info()['brand_raw']
     if "amd" in brand.lower():
         return True
     else:
